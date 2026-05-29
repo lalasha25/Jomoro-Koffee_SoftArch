8ed5c51332afceb26eb27ac7e2d89e0ad8775814
lia7277 <ceciliaeuginia68532@gmail.com>
2026-05-29 09:15:25 +0700

    feat: add auth service with JWT, register, login, profile

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    PrismaModule,
+    ProductsModule,
+    CategoriesModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
